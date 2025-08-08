56a8d986bc688b710736175bc8a1f9c7523ca0f0
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-08 12:48:18 +0530

    implement iter consumer and adaptor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+fn print_elements(elements: &Vec<String>){
+    // for element in elements {
+    //     println!("{}", element);
+    // }
+
+    elements.iter()
+        .map(|el| format!("{} {}", el, el))
+        .for_each(|value| println!("{}", value));
+}
+
 fn main() {
     let colors = vec![
         String::from("Red"),
@@ -5,6 +16,9 @@ fn main() {
         String::from("Green"),
         String::from("Orange"),
     ];
+
+    print_elements(&colors);
+
     let mut color_iter = colors.iter();
     println!("{:#?}",color_iter.next());
     println!("{:#?}",color_iter.next());
