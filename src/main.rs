e60baababe9e1e4b164943c23b6160b9658f514e
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-08 12:03:08 +0530

    basic implementation of iterator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![
+        String::from("Red"),
+        String::from("Blue"),
+        String::from("Green"),
+        String::from("Orange"),
+    ];
+    let mut color_iter = colors.iter();
+    println!("{:#?}",color_iter.next());
+    println!("{:#?}",color_iter.next());
+    println!("{:#?}",color_iter.next());
+    println!("{:#?}",color_iter.next());
+    println!("{:#?}",color_iter.next());
 }
