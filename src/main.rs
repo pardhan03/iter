e0ea83f80bd3ff0110b2c56d121d6dd610af3596
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-11 12:33:14 +0530

    implement different types of iterator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,25 @@ fn print_elements(elements: &Vec<String>){
         .for_each(|value| println!("{}", value));
 }
 
+fn shorten_strings(elements: &mut [String]) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
+
+fn to_uppercase(elements: &mut [String]) -> Vec<String> {
+    elements
+        .iter()
+        .map(|el| el.to_uppercase())
+        .collect()
+}
+
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el))
+}
+
+fn explode(elements: &[String]) -> Vec<Vec<String>> {
+    elements.iter().map(|el| el.chars().map(|ch| ch.to_string()).collect()).collect()
+}
+
 fn main() {
     let colors = vec![
         String::from("Red"),
@@ -17,12 +36,25 @@ fn main() {
         String::from("Orange"),
     ];
 
-    print_elements(&colors);
+    // print_elements(&colors);
 
-    let mut color_iter = colors.iter();
-    println!("{:#?}",color_iter.next());
-    println!("{:#?}",color_iter.next());
-    println!("{:#?}",color_iter.next());
-    println!("{:#?}",color_iter.next());
-    println!("{:#?}",color_iter.next());
+    // let mut color_iter = colors.iter();
+    // println!("{:#?}",color_iter.next());
+    // println!("{:#?}",color_iter.next());
+    // println!("{:#?}",color_iter.next());
+    // println!("{:#?}",color_iter.next());
+    // println!("{:#?}",color_iter.next());
+
+    // shorten_strings(&mut colors[1..3]);
+    // println!("{:#?}", colors)
+
+    // let uppercases = to_uppercase(&mut colors[1..3]);
+    // println!("{:#?}", uppercases)
+
+    // let mut destination = vec![];
+    // move_elements(colors, &mut destination);
+    // println!("Destination: {:#?}", destination);
+
+    let exploded = explode(&colors);
+    println!("{:#?}", exploded);
 }
